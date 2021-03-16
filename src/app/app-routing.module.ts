dbf8470e8b0277ff047b4d0a1fcc4afeb11af198
Alexander Yakovlev <sashayassa@gmail.com>
2021-03-16 10:40:28 +0300

    update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {GreetingComponent} from "./greeting/greeting.component";
+import {CoursesComponent} from "./courses/courses.component";
+import {NotFoundComponent} from "./not-found/not-found.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: GreetingComponent },
+  { path: 'courses', component: CoursesComponent },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
