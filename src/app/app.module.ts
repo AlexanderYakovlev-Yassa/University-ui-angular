046398cb52d06932272b527c6206aff7bea60a7f
Alexander Yakovlev <sashayassa@gmail.com>
2021-03-20 09:01:35 +0300

    updating app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { CoursesComponent } from './courses/courses.component';
-import { CourseCardComponent } from './courses/course-card/course-card.component';
-import { CourseDetailComponent } from './courses/course-card/course-detail/course-detail.component';
 import { GreetingComponent } from './greeting/greeting.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { FooterComponent } from './footer/footer.component';
-import { CourseEditComponent } from './courses/course-card/course-edit/course-edit.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     CoursesComponent,
-    CourseCardComponent,
-    CourseDetailComponent,
     GreetingComponent,
     NotFoundComponent,
-    FooterComponent,
-    CourseEditComponent
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
