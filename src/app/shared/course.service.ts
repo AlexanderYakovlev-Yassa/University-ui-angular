3a2b5c614ed5355b868a6025c0b6f71b064d23a9
Alexander Yakovlev <sashayassa@gmail.com>
2021-03-20 09:10:13 +0300

    updating course service

--- src/app/shared/course.service.ts
+++ src/app/shared/course.service.ts
@@ -1,9 +1,9 @@
-import {Injectable} from "@angular/core";
-import {HttpClient, HttpHeaders} from "@angular/common/http";
-import {Observable, of} from "rxjs";
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable, of} from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
-import {MessageService} from "./message.service";
-import {environment} from "../../environments/environment";
+import {MessageService} from './message.service';
+import {environment} from '../../environments/environment';
 
 export interface Course {
   id: number;
@@ -14,24 +14,18 @@ export interface Course {
 const httpOptions = {
   headers: new HttpHeaders({
     'Content-Type':  'application/json'
-    //,Authorization: 'my-auth-token'
+    // ,Authorization: 'my-auth-token'
   })
 };
-const url: string = environment.apiUrl + "courses";
+const url: string = environment.apiUrl + 'courses';
 
-@Injectable({providedIn: "root"})
+@Injectable({providedIn: 'root'})
 export class CourseService {
 
   constructor(
     private http: HttpClient,
     private messageService: MessageService
-  ) {};
-
-  public courses: Course[] = [
-    {id: 1, name: "Математика", description: "Курс математики"},
-    {id: 1, name: "Геометрия", description: "Курс геометрии"},
-    {id: 1, name: "Астрономия", description: "Курс астрономии"}
-  ];
+  ) {}
 
   getCourses(): Observable<Course[]> {
     return this.http.get<Course[]>(url)
@@ -42,7 +36,7 @@ export class CourseService {
   }
 
   updateCourse(course: Course): Observable<Course> {
-    const updateUrl: string = url + "/" + course.id;
+    const updateUrl: string = url + '/' + course.id;
 
     return this.http.put<Course>(updateUrl, course, httpOptions)
       .pipe(
@@ -51,13 +45,20 @@ export class CourseService {
       );
   }
 
-  createCourse(course: Course) {
+  createCourse(course: Course): Observable<Course> {
 
     return this.http.post<Course>(url, course, httpOptions)
       .pipe(
         tap(_ => this.log('fetched course')),
         catchError(this.handleError<Course>('createCourse', course))
       );
+  }
+
+  deleteCourse(id: number): Observable<void> {
+
+    const deleteUrl: string = url + '/' + id;
+
+    return this.http.delete<void>(deleteUrl);
   }
 
   /**
