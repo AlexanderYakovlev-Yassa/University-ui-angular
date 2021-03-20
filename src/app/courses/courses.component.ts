692fddb12a727643014a901c08cd067d658da102
Alexander Yakovlev <sashayassa@gmail.com>
2021-03-20 09:10:58 +0300

    updating course.component files

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {Course, CourseService} from "../shared/course.service";
-import {delay} from "rxjs/operators";
+import {Course, CourseService} from '../shared/course.service';
+import {FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-courses',
@@ -9,8 +9,11 @@ import {delay} from "rxjs/operators";
 })
 export class CoursesComponent implements OnInit {
 
+  id: FormControl = new FormControl(0);
+  name: FormControl = new FormControl('');
+  description: FormControl = new FormControl('');
   courses: Course[] = [];
-  isLoaded: boolean = false;
+  isLoaded = false;
 
   constructor(private courseService: CourseService) {
   }
@@ -28,7 +31,51 @@ export class CoursesComponent implements OnInit {
       });
   }
 
-  createCourse() {
+  selectCourse(course: Course): void {
 
+    this.id.setValue(course.id);
+    this.name.setValue(course.name);
+    this.description.setValue(course.description);
+  }
+
+  createCourse(): void {
+
+    const newCourse: Course = {id: 0, name: this.name.value, description: this.description.value};
+
+    this.courseService.createCourse(newCourse)
+      .subscribe(c => {
+        this.name.setValue(c.name);
+        this.description.setValue(c.description);
+        this.getCourses();
+      });
+  }
+
+  updateCourse(): void {
+
+    const modifiedCourse: Course = {id: this.id.value, name: this.name.value, description: this.description.value};
+
+    this.courseService.updateCourse(modifiedCourse)
+      .subscribe(c => {
+        this.name.setValue(c.name);
+        this.description.setValue(c.description);
+        this.getCourses();
+      });
+  }
+
+  deleteCourse(): void {
+
+    const id: number = this.id.value;
+
+    this.courseService.deleteCourse(id)
+      .subscribe(() => {
+        this.clearForm();
+        this.getCourses();
+      });
+  }
+
+  private clearForm(): void {
+    this.id.setValue(0);
+    this.name.setValue('');
+    this.description.setValue('');
   }
 }
